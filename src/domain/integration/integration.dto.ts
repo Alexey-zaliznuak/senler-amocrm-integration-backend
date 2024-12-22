bf4336f04bc1a5173bf0c3761b1713d1575319d2
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-12-22 19:12:14 +0300

    annotate bot step webhook

--- src/domain/integration/integration.dto.ts
+++ src/domain/integration/integration.dto.ts
@@ -48,17 +48,5 @@ export class LeadDto {
 
   @IsObject()
   @IsNotEmpty()
-  personalVars: Record<string, string | number | boolean>; // TODO check for empty vars
+  personalVars: Array<void> | Record<string, string | number | boolean>;
 }
-
-// "integrationSecret": "85f346fa12cf86efa4ebc48ba6e9f2f79ab78665bb86d91d6bbef25431828a5c",
-// backend-1  |             "publicIntegrationInfo": "{\"publicText\":\"qwe\",\"token\":\"type\",\"type\":\"qw\"}",
-// backend-1  |             "user": {
-// backend-1  |                 "leadId": "66db1078287a7ca40c579097",
-// backend-1  |                 "name": "Алексей",
-// backend-1  |                 "personalVars": {
-// backend-1  |                     "lolol": 1234,
-// backend-1  |                     "xtime": 138
-// backend-1  |                 },
-// backend-1  |                 "surname": "Зализняк"
-// backend-1  |             }
