373f9ffcda78fa11e3af4ffffd00a55539c325f4
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-12-22 19:14:54 +0300

    clear rubbish

--- src/domain/integration/integration.service.ts
+++ src/domain/integration/integration.service.ts
@@ -4,8 +4,4 @@ import { prisma } from 'src/infrastructure/database';
 @Injectable()
 export class IntegrationService {
     constructor() {}
-
-    async test() {
-        await prisma.lead.findUnique()
-    }
 }
