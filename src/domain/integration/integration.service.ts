31be08d24ca5d90330c278df15088e6ff1557324
maxi-q <maximkabust@gmail.com>
2025-01-10 15:49:49 +0300

    .

--- src/domain/integration/integration.service.ts
+++ src/domain/integration/integration.service.ts
@@ -1,38 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { SenlerApiClient } from 'senler-sdk';
 import { AmoCrmService } from 'src/external/amo-crm';
 import { prisma } from 'src/infrastructure/database';
+import { CustomRequest } from 'src/infrastructure/requests';
+import { BotStepWebhookDto, BotStepType } from './integration.dto';
 
 @Injectable()
 export class IntegrationService {
   constructor(private readonly amoCrmService: AmoCrmService) {}
 
-  // async processBotStepWebhook(req: CustomRequest, body: BotStepWebhookDto) {
-  //   // create lead if not exists
-  //   const lead = await prisma.lead.findUniqueOrThrow({
-  //     where: { senlerLeadId: body.lead.id },
-  //     select: { senlerGroup: true },
-  //   });
+  async processBotStepWebhook(req: CustomRequest, body: BotStepWebhookDto) {
+    // create lead if not exists
+    const lead = await prisma.lead.findUniqueOrThrow({
+      where: { senlerLeadId: body.lead.id },
+      select: { senlerGroup: true },
+    });
 
-  //   const senlerClient = new SenlerApiClient({
-  //     accessToken: lead.senlerGroup.senlerAccessToken,
-  //     vkGroupId: lead.senlerGroup.senlerVkGroupId,
-  //   });
+    const senlerClient = new SenlerApiClient({
+      accessToken: lead.senlerGroup.senlerAccessToken,
+      vkGroupId: lead.senlerGroup.senlerVkGroupId,
+    });
 
-  //   if (body.publicBotStepSettings.type == BotStepType.SendDataToAmoCrm) {
-  //     const senlerVariables =
-  //       body.publicBotStepSettings.syncableVariables.forEach(
-  //         (value, _index) => value.from,
-  //       );
-  //     //await senlerClient.vars.get({vk_user_id: body.lead.vkUserId})
-  //     // console.info(temp)
-  //     return {};
-  //   }
-  //   if (body.publicBotStepSettings.type == BotStepType.SendDataToSenler) {
-  //     // const _amoCrmVariables = body.publicBotStepSettings.syncableVariables.forEach(
-  //     //     (value, _index) => value.from,
-  //     //   );
-  //   }
-  // }
+    if (body.publicBotStepSettings.type == BotStepType.SendDataToAmoCrm) {
+      const senlerVariables =
+        body.publicBotStepSettings.syncableVariables.forEach(
+          (value, _index) => value.from,
+        );
+      //await senlerClient.vars.get({vk_user_id: body.lead.vkUserId})
+      // console.info(temp)
+      return {};
+    }
+    if (body.publicBotStepSettings.type == BotStepType.SendDataToSenler) {
+      // const _amoCrmVariables = body.publicBotStepSettings.syncableVariables.forEach(
+      //     (value, _index) => value.from,
+      //   );
+    }
+  }
   // publicBotStepSettings
 
   async createLeadIfNotExists({
