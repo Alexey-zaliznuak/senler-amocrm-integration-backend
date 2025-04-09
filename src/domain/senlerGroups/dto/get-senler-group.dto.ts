e2957ab7dce7b10e3453406baacc928a3da0100a
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-04-09 12:39:13 +0300

    updates

--- src/domain/senlerGroups/dto/get-senler-group.dto.ts
+++ src/domain/senlerGroups/dto/get-senler-group.dto.ts
@@ -1,8 +1,10 @@
-import { GetIntegrationStepTemplateResponseDto } from './../../integrationStepTemplates/dto/get-integration-step-template.dto';
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
-import { BaseSenlerGroup, BaseSenlerGroupDto } from './basic-senler-group.dto';
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested } from 'class-validator';
 import { GetIntegrationStepTemplateResponse } from 'src/domain/integrationStepTemplates/dto/get-integration-step-template.dto';
+import { GetIntegrationStepTemplateResponseDto } from './../../integrationStepTemplates/dto/get-integration-step-template.dto';
+import { BaseSenlerGroup, BaseSenlerGroupDto } from './basic-senler-group.dto';
 
 export type SenlerGroupFieldForGetByUniqueField = keyof Pick<Prisma.SenlerGroupWhereUniqueInput, 'id' | 'senlerGroupId'>;
 export const SenlerGroupNumericFieldsForGetByUniqueFields = ['senlerGroupId'];
@@ -12,13 +14,19 @@ export enum SenlerGroupFieldForGetByUniqueFieldEnum {
 }
 
 export type GetSenlerGroupResponse = Pick<BaseSenlerGroup, 'id' | 'amoCrmDomainName' | 'senlerGroupId'> & {
-  integrationStepTemplates: GetIntegrationStepTemplateResponse[]
-}
+  integrationStepTemplates: GetIntegrationStepTemplateResponse[];
+};
 
-export class GetSenlerGroupResponseDto extends PickType(BaseSenlerGroupDto, [
-  'id',
-  'amoCrmDomainName',
-  'senlerGroupId',
-] as const) implements GetSenlerGroupResponse {
-  integrationStepTemplates: GetIntegrationStepTemplateResponseDto[]
+export class GetSenlerGroupResponseDto
+  extends PickType(BaseSenlerGroupDto, ['id', 'amoCrmDomainName', 'senlerGroupId'] as const)
+  implements GetSenlerGroupResponse
+{
+  @ApiProperty({
+    description: 'Templates',
+    type: () => [GetIntegrationStepTemplateResponseDto], // Указываем тип массива
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => GetIntegrationStepTemplateResponseDto)
+  integrationStepTemplates: GetIntegrationStepTemplateResponseDto[];
 }
