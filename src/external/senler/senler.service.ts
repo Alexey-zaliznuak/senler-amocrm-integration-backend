8aa54b6440608c4fa1b090cb7c5c40e7253a5256
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-05-03 23:09:50 +0300

    staged

--- src/external/senler/senler.service.ts
+++ src/external/senler/senler.service.ts
@@ -20,7 +20,7 @@ export class SenlerService {
   async acceptWebhookRequest(body: BotStepWebhookDto): Promise<void> {
     this.logger.info("Секретный ключ интеграции: "+ body.integrationSecret)
     const {group_id, ...botCallback} = body.botCallback
-    const hash = this.generateHash(botCallback, body.integrationSecret)
+    const hash = this.generateHash({...botCallback, group_id}, body.integrationSecret)
 
     await this.sendRequest({ url: this.callbackUrl, params: { hash, group_id, bot_callback: botCallback } });
   }
