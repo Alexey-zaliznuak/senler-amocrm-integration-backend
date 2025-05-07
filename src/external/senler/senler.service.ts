b0c31dbfeb5e3b36d150dcf642feda34b967a788
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-05-07 14:12:33 +0300

    staged

--- src/external/senler/senler.service.ts
+++ src/external/senler/senler.service.ts
@@ -32,7 +32,7 @@ export class SenlerService {
 
   private generateHash(body: BotStepWebhookDto['botCallback'], secret: string) {
     const {group_id, ...bodyForHash} = body
-    const forHash = `${body.group_id}${JSON.stringify(bodyForHash)}${secret}`;
+    const forHash = `${body.group_id}${JSON.stringify(bodyForHash).replace(/:/g, ": ").replace(/,/g, ", ")}${secret}`;
 
     this.logger.info(`Тело для хеша:`, bodyForHash);
     this.logger.info(`Строка для хеша: ${forHash}`);
