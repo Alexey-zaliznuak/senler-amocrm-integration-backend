8859a6d6682af1089e844a9b99831bccf2cac577
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-05-17 20:38:43 +0300

    staged

--- src/infrastructure/config/config.validation-schema.ts
+++ src/infrastructure/config/config.validation-schema.ts
@@ -20,8 +20,8 @@ export const appConfigValidationSchema = Joi.object({
   RABBITMQ_TRANSFER_QUEUE: Joi.string().required(),
   RABBITMQ_TRANSFER_ROUTING_KEY: Joi.string().required(),
 
-  TRANSFER_MESSAGE_MAX_RETRY_DELAY: Joi.number().default(AppConfig.TRANSFER_MESSAGE_MAX_RETRY_DELAY),
-  TRANSFER_MESSAGE_RETRY_DELAY_BASE: Joi.number().default(AppConfig.TRANSFER_MESSAGE_RETRY_DELAY_BASE),
+  TRANSFER_MESSAGE_MAX_RETRY_DELAY: Joi.number().required(),
+  TRANSFER_MESSAGE_RETRY_DELAY_BASE: Joi.number().required(),
 
   CACHE_DATABASE_URL: Joi.string().uri().required(),
   CACHE_DEFAULT_TTL: Joi.number().required(),
