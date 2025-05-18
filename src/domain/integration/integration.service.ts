7a287aaadb53d742b54f825fe59a7fbc2fdbd83e
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-05-18 17:16:09 +0300

    remove rubbish loggers

--- src/domain/integration/integration.service.ts
+++ src/domain/integration/integration.service.ts
@@ -167,17 +167,10 @@ export class IntegrationService {
         tokens,
       });
 
-      this.logger.info('DDDDDDDDDDDDDD', {
-        1: payload,
-        2: payload.publicBotStepSettings.type == BotStepType.SendDataToAmoCrm,
-        3: payload.publicBotStepSettings.type == BotStepType.SendDataToSenler,
-      });
       if (payload.publicBotStepSettings.type == BotStepType.SendDataToAmoCrm) {
-        this.logger.info('DDDDDDDDDDDDDD AMO CRM');
         await this.sendVarsToAmoCrm(payload, tokens, lead);
       }
       if (payload.publicBotStepSettings.type == BotStepType.SendDataToSenler) {
-        this.logger.info('DDDDDDDDDDDDDD Senler');
         await this.sendVarsToSenler(payload, amoCrmLead, senlerGroup.amoCrmAccessToken);
       }
 
@@ -250,7 +243,6 @@ export class IntegrationService {
       body.lead.personalVars
     );
 
-    this.logger.info('ОТПРАВКА ДАННЫХ В АМО');
     await this.amoCrmService.editLeadsById({
       amoCrmDomainName: lead.senlerGroup.amoCrmDomainName,
       amoCrmLeadId: lead.amoCrmLeadId,
