b1c968711b4bcf907bcdf1fe46d77f686830f23a
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-05-27 20:03:47 +0300

    test

--- src/external/amo-crm/handlers/rate-limit.decorator.ts
+++ src/external/amo-crm/handlers/rate-limit.decorator.ts
@@ -19,11 +19,8 @@ export function UpdateRateLimitAndThrowIfNeed(increment: number = 1) {
 
     descriptor.value = async function (...args: any[]) {
       const className = target.prototype ? target.name : target.constructor.name;
-      try{
-        await this.rateLimitsService.updateRateLimitAndThrowIfNeed(args[0].amoCrmDomainName, increment);
-      } catch {
-        new LoggingService(AppConfig).createLogger().info(`Декоратор не применяется к методу ${propertyKey} в классе ${className}`);
-      }
+      new LoggingService(AppConfig).createLogger().info(`Декоратор применяется к методу ${propertyKey} в классе ${className}`);
+      await this.rateLimitsService.updateRateLimitAndThrowIfNeed(args[0].amoCrmDomainName, increment);
       return await originalMethod.apply(this, args);
     };
 
