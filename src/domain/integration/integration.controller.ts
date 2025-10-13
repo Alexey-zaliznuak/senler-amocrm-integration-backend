fdf8a8a622c7ee9f2a2966d24dbf50bb5c2957d4
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-10-13 16:55:07 +0300

    затычка для вебхука

--- src/domain/integration/integration.controller.ts
+++ src/domain/integration/integration.controller.ts
@@ -17,12 +17,17 @@ import {
 export class IntegrationController {
   constructor(private readonly integrationService: IntegrationService) {}
 
+  // @Post('/botStepWebhook')
+  // @HttpCode(HttpStatus.OK)
+  // @UseGuards(IntegrationSecretGuard)
+  // @ApiBody({ type: BotStepWebhookDto })
+  // async botStepWebhook(@Body() body: any): Promise<any> {
+  //   return await this.integrationService.processBotStepWebhook(body);
+  // }
+
   @Post('/botStepWebhook')
-  @HttpCode(HttpStatus.OK)
-  @UseGuards(IntegrationSecretGuard)
-  @ApiBody({ type: BotStepWebhookDto })
   async botStepWebhook(@Body() body: any): Promise<any> {
-    return await this.integrationService.processBotStepWebhook(body);
+    return ""
   }
 
   @Delete('/change-amocrm-account')
