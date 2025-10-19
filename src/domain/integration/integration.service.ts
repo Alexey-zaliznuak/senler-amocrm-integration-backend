f38b548543cd900cd9593df1fe6f7b2487d13274
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-10-19 17:07:00 +0300

    add config get route for debugging

--- src/domain/integration/integration.service.ts
+++ src/domain/integration/integration.service.ts
@@ -16,7 +16,7 @@ import { AmoCrmService } from 'src/external/amo-crm';
 import { AmoCrmError, AmoCrmExceptionType, GetLeadResponse as AmoCrmLead, AmoCrmTokens } from 'src/external/amo-crm/amo-crm.dto';
 import { RateLimitsService } from 'src/external/amo-crm/rate-limit.service';
 import { SenlerService } from 'src/external/senler/senler.service';
-import { AppConfigType } from 'src/infrastructure/config/config.app-config';
+import { AppConfig, AppConfigType } from 'src/infrastructure/config/config.app-config';
 import { CONFIG } from 'src/infrastructure/config/config.module';
 import { PRISMA } from 'src/infrastructure/database/database.config';
 import { PrismaExtendedClientType } from 'src/infrastructure/database/database.service';
@@ -48,7 +48,7 @@ export class IntegrationService {
 
   // TODO удалить
   public getConfig() {
-    return this.config;
+    return AppConfig;
   }
 
   async changeAmoCrmAccount(body: ChangeAmoCrmAccountRequestDto): Promise<void> {
