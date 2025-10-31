ec23c197cfb561e41275d3905b6f78db8a7adff0
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-11-01 00:56:08 +0300

    PLG-120 - отображение Invalid Type ошибок

--- src/external/amo-crm/amo-crm.service.ts
+++ src/external/amo-crm/amo-crm.service.ts
@@ -544,7 +544,7 @@ export class AmoCrmService {
 
               for (let field of leadFields) {
                 if (failedField.field_id === field.id) {
-                  variableName = `поле: '${field.name}'` || `переменной с ID ${failedField.field_id}`;
+                  variableName = `поле: «${field.name}»` || `переменной с ID ${failedField.field_id}`;
                 }
               }
             } catch (e) {}
